d2610fb6bbb4dc0bdf1a178f28f571342ff3b7e9
kobeydon <pacemaking313@gmail.com>
2019-03-27 20:22:22 +0900

    add User Registration

--- poddict/register/forms.py
+++ poddict/register/forms.py
@@ -1,6 +1,10 @@
+from django import forms
 from django.contrib.auth.forms import(
     AuthenticationForm
 )
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class LoginForm(AuthenticationForm):
@@ -11,3 +15,19 @@ class LoginForm(AuthenticationForm):
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
             field.widget.attrs['placeholder'] = field.label
+
+class UserCreateForm(UserCreationForm):
+    """Form to Create User"""
+
+    class Meta:
+        model = User
+        if User.USERNAME_FIELD == 'email':
+            fields = ('email',)
+        else:
+            fields = ('username', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            filed.widget.attrs['class'] = 'form-control'
+            
--- poddict/register/views.py
+++ poddict/register/views.py
@@ -1,10 +1,89 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import (
     LoginView, LogoutView
 )
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.signing import BadSignature, SignatureExpired, loads, dumps
+from django.http import Http404, HttpResponseBadRequest
+from django.shortcuts import redirect
+from django.template.loader import get_template
 from django.views import generic
-from .forms import LoginForm
+from .forms import (
+    LoginForm, UserCreateForm
+)
 
+
+User = get_user_model()
+
+
+
+class UserCreate(generic.CreateView):
+    """temporary registration"""
+    template_name = 'register/user_create.html'
+    form_class = UserCreateForm
+
+    def form_valid(self, form):
+    """temporarily register and send activation mail"""
+    user = form.save(commit=False)
+    user.is_active = False
+    user.save()
+
+    """ send activation url """
+    current_site = get_current_site(self.request)
+    domain = current_site.domain
+    context = {
+        'protocol':self.request.scheme,
+        'domain':domain,
+        'token':dumps(user.pk),
+        'user':user,
+    }
+
+    subject_template = get_template('register/mail_template/create/subject.txt')
+    subject = subject_template.render(context)
+
+    message_template = get_template('register/mail_template/create/message.txt')
+    message = message_template.render(context)
+
+    user.email_user(subject, message)
+    return redirect('register:user_create_done')
+
+
+class UserCreateDone(generic.TemplateView):
+    """ User temporary registration is done """
+    template_name = 'register/user_create_done.html'
+
+class UserCreateComplete(generic.TemplateView):
+    """Officially register new user when activate by clicking the link in the Email"""
+    template_name = 'register/user_create_complete.html'
+    timeout_seconds = getattr(settings, 'ACTIVATION_TIMEOUT_SECONDS', 60*60*24)
+
+    def get(self,request,**kwargs):
+        """register if token is correct"""
+        token = kwargs.get('token')
+        try:
+            user_pk = loads(token, max_age=self.timeout_seconds)
+
+        except SignatureExpired:
+            return HttpResponseBadRequest()
+
+        except BadSignature:
+            return HttpResponseBadRequest()
+
+        else:
+            try:
+                user = User.objects.get(pk=user_pk)
+            except User.DoesNotExist:
+                return HttpResponseBadRequest()
+            else:
+                if not user.is_active:
+                    user.is_active = True
+                    user.save()
+                    return super().get(request, **kwargs)
+
+            return HttpResponseBadRequest()
+            
 
 class Top(generic.TemplateView):
     template_name = 'register/top.html'
